80813848c049071599e71b2986a1e15c063a4610
AndBuh89 <andreea@milaman.ro>
2026-01-04 18:59:27 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,34 +21,12 @@ export default function RootLayout({ children }) {
 <Script id="tiktok-pixel" strategy="afterInteractive">
 {`
 !function (w, d, t) {
-  w.TiktokAnalyticsObject = t;
-  var ttq = w[t] = w[t] || [];
-  ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-  ttq.setAndDefer = function(t, e) {
-    t[e] = function() {
-      t.push([e].concat(Array.prototype.slice.call(arguments, 0)))
-    }
-  };
-  for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
-
-  ttq.load = function(e, n) {
-    var r = "https://analytics.tiktok.com/i18n/pixel/events.js", o = n && n.partner;
-    ttq._i = ttq._i || {};
-    ttq._i[e] = [];
-    ttq._i[e]._u = r;
-    ttq._t = ttq._t || {};
-    ttq._t[e] = +new Date;
-    ttq._o = ttq._o || {};
-    ttq._o[e] = n || {};
-    var s = d.createElement("script");
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = r + "?sdkid=" + e + "&lib=" + t;
-    var x = d.getElementsByTagName("script")[0];
-    x.parentNode.insertBefore(s, x);
-  };
-
-  ttq.load('D5CN6NRC77U3FL790S20');
+  w.TiktokAnalyticsObject=t;
+  var ttq=w[t]=w[t]||[];
+  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+  ttq.load('D5D9GD3C77U15BU61UUG');
   ttq.page();
 }(window, document, 'ttq');
 `}
