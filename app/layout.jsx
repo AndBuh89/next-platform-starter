4b9920b5d15f72f975965c2559962b7ff37f9f27
AndBuh89 <andreea@milaman.ro>
2026-01-04 19:52:04 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,4 @@
-import '../styles/globals.css'
-import { Footer } from '../components/footer'
-import { Header } from '../components/header'
-
-export const metadata = {
-  title: {
-    template: '%s | VitalFlow',
-    default: 'VitalFlow'
-  }
-}
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head />
-
-      <body className="antialiased text-white bg-blue-900">
-
-
-        import "../styles/globals.css";
+import "../styles/globals.css";
 import Script from "next/script";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
@@ -30,18 +11,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="antialiased text-white bg-blue-900">
+        {/* TIKTOK PIXEL */}
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
 !function (w, d, t) {
   w.TiktokAnalyticsObject = t;
   var ttq = w[t] = w[t] || [];
   ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
-  ttq.setAndDefer = function(t, e) { t[e] = function() { t.push([e].concat(Array.prototype.slice.call(arguments, 0))); } };
+  ttq.setAndDefer = function(t, e) {
+    t[e] = function() {
+      t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+    };
+  };
   for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+
   ttq.instance = function(t) {
     for (var e = ttq._i[t] || [], n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]);
     return e;
   };
+
   ttq.load = function(e, n) {
     var r = "https://analytics.tiktok.com/i18n/pixel/events.js", o = n && n.partner;
     ttq._i = ttq._i || {};
@@ -64,6 +52,7 @@ export default function RootLayout({ children }) {
 }(window, document, "ttq");
           `}
         </Script>
+        {/* END PIXEL */}
 
         <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
           <div className="flex flex-col w-full max-w-5xl mx-auto grow">
